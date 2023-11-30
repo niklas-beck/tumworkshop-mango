0cf7d67286b1930c03ccbee71df30c7f5f34e107
xingggu <108153474+xingggu@users.noreply.github.com>
2023-11-30 18:48:06 +0100

    Update function_app.py

--- src/function_app.py
+++ src/function_app.py
@@ -27,5 +27,24 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
 
 
 # Add a new function here
+def http_trigger2(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    host = req.params.get('host')
+    if not host:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            host = req_body.get('host')
+
+    if host:
+        return func.HttpResponse(f"Hello, {host}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. But who are you?",
+             status_code=200
+        )
 
 # TODO
